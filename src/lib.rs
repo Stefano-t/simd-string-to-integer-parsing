e4fd6ef38c0c20f16e22add66da35905869f0b66
Stefano Taverni <ste.taverni@gmail.com>
2021-10-25 17:21:48 +0200

    Conditionally compile `parse_integer`

--- src/lib.rs
+++ src/lib.rs
@@ -52,23 +52,34 @@ pub fn check_all_chars_are_valid(s: &str) -> bool {
 /// integer.  If the string has at least 16 chars, then it can perform parsing
 /// exploiting the SIMD intrinsics.
 pub fn parse_integer(s: &str, separator: u8, eol: u8) -> Option<u32> {
-    if s.len() < VECTOR_SIZE {
-        return fallback::parse_integer_byte_iterator(s, separator, eol);
+    #[inline]
+    #[cfg(all(not(target_feature = "sse4.2"), not(target_feature = "sse2")))]
+    unsafe fn imp_fn(s: &str, separator: u8, eol: u8) -> Option<u32> {
+        fallback::parse_integer_byte_iterator(s, separator, eol);
     }
-    // find the first occurence of a separator
-    let (index, mask) = last_byte_digit(s, separator, eol);
-    match index {
-        8 => return Some(sse2::parse_8_chars_simd(s)),
-        10 => return Some(sse2::parse_more_than_8_simd(s, 1000000, mask)),
-        9 => return Some(sse2::parse_more_than_8_simd(s, 10000000, mask)),
-        7 => return Some(sse2::parse_less_than_8_simd(s, 10, mask)),
-        6 => return Some(sse2::parse_less_than_8_simd(s, 100, mask)),
-        5 => return Some(sse2::parse_less_than_8_simd(s, 1000, mask)),
-        4 => return Some(sse2::parse_less_than_8_simd(s, 10000, mask)),
-        1..=3 => return Some(fallback::parse_byte_iterator_limited(s, index)),
-        // all the chars are numeric, maybe padded?
-        32 => return Some(sse2::parse_integer_simd_all_numbers(s)),
-        // there is no u32 to parse
-        _ => return None,
+
+    #[cfg(any(target_feature = "sse4.2", target_feature = "sse2"))]
+    unsafe fn imp_fn(s: &str, separator: u8, eol: u8) -> Option<u32> {
+        if s.len() < VECTOR_SIZE {
+            return fallback::parse_integer_byte_iterator(s, separator, eol);
+        }
+        // find the first occurence of a separator
+        let (index, mask) = last_byte_digit(s, separator, eol);
+        match index {
+            8 => return Some(sse2::parse_8_chars_simd(s)),
+            10 => return Some(sse2::parse_more_than_8_simd(s, 1000000, mask)),
+            9 => return Some(sse2::parse_more_than_8_simd(s, 10000000, mask)),
+            7 => return Some(sse2::parse_less_than_8_simd(s, 10, mask)),
+            6 => return Some(sse2::parse_less_than_8_simd(s, 100, mask)),
+            5 => return Some(sse2::parse_less_than_8_simd(s, 1000, mask)),
+            4 => return Some(sse2::parse_less_than_8_simd(s, 10000, mask)),
+            1..=3 => return Some(fallback::parse_byte_iterator_limited(s, index)),
+            // all the chars are numeric, maybe padded?
+            32 => return Some(sse2::parse_integer_simd_all_numbers(s)),
+            // there is no u32 to parse
+            _ => return None,
+        }
     }
+
+    unsafe { imp_fn(s, separator, eol) }
 }
